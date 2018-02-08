9eae41a74c05cfd518a4db2896d3e72e3c95bfa7
Jayson <jayson@mac.local>
2018-02-08 17:02:35 +0800

    react mapbox

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import mapboxgl from 'mapbox-gl';
+import { token } from './credential';
 
 class App extends Component {
+
+  componentDidMount() {
+    mapboxgl.accessToken = token;
+
+    new mapboxgl.Map({
+      container: this.mapContainer, //'<your HTML element id>',
+      style: 'mapbox://styles/mapbox/streets-v9'
+    });
+  }
+
   render() {
+    const divStyle = {
+      height: '400px',
+      width: '400px'
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div ref={el => this.mapContainer = el} style={divStyle}></div>
       </div>
     );
   }
